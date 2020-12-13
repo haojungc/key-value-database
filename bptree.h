6639aab8b042b0da0c399ded1f2ca61c538de31f
Nick Chen <nickchenchj@gmail.com>
2020-12-13 21:53:26 +0800

    Add comments to bptree.h

--- bptree.h
+++ bptree.h
@@ -18,18 +18,32 @@ typedef struct node {
 } node_t;
 
 typedef struct bptree {
+    /* Loads records from file. */
     void (*load)(const char *filepath, const uint64_t total_keys);
+    /* Saves the B+ tree to file and updates metadata. */
     void (*save)(metadata_t *metadata, const char *filepath);
+    /* Splits the B+ tree into two parts and saves one of them to file,
+     * according to key. */
     void (*split_and_save_one)(metadata_t *metadata, const char *filepath,
                                const uint64_t key);
+    /* Frees the memory allocated for the B+ tree. */
     void (*free_memory)();
+    /* Inserts a record into the B+ tree. */
     void (*insert)(const uint64_t key, char *value);
+    /* Searches key in the B+ tree. */
     const char *(*search)(const uint64_t key);
+    /* Scans from start key to end key and assigns the address of the value (if
+     * found) to the pointer array. Note that the pointer array is initialized
+     * to NULL before passing it to this function. */
     void (*scan)(char *ptrs[], const uint64_t start_key,
                  const uint64_t end_key);
+    /* Returns a non zero value if the tree is empty, and 0 otherwise. */
     int_fast8_t (*is_empty)();
+    /* Returns a non zero value if the tree is full, and 0 otherwise. */
     int_fast8_t (*is_full)();
+    /* Returns the minimum key in the tree. */
     uint64_t (*get_min_key)();
+    /* Returns the maximum key in the tree. */
     uint64_t (*get_max_key)();
     // void (*check)();
     // void (*show)();
